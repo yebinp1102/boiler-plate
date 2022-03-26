b00db8190f053e3916222b937734c0a82e25667b
yebin Park <yebinp1102@naver.com>
2022-03-26 18:19:03 +0900

    mongoDB security updated

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const config = require('./config/key')
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://yebinp1102:qkrdPqls1102!@online-store.bfj9z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
   .then(()=>console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
